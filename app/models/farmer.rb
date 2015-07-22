9fd5562a3fc88c2605ffe052bf7088c6e25ecbd7
pracdev <danielcomet819@gmail.com>
2015-07-21 20:32:02 -0400

    Inserted the validation...

--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -1,2 +1,18 @@
 class Farmer < ActiveRecord::Base
+  attr_accessible :email, :farm, :name, :produce, :produce_price, :wepay_access_token, :wepay_account_id, :password
+
+  validates :password, :presence => true
+  validates :password, :length => { :in => 6..200}
+  validates :name, :email, :presence => true
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :format => { :with => /@/, :message => " is invalid" }
+
+  def password
+    password_hash ? @password ||= BCrypt::Password.new(password_hash) : nil
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.password_hash = @password
+  end
 end
