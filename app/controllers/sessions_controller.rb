2682277531c5123a395e552b76bedc297c805ea2
pracdev <danielcomet819@gmail.com>
2015-07-21 20:55:22 -0400

    Session controller is created...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  # POST /sessions
+  def create
+    farmer = Farmer.authenticate params[:email], params[:password]
+      if farmer
+        session[:farmer_id] = farmer.id
+        redirect_to root_path, :notice => "Welcome back to WeFarm"
+      else
+        redirect_to :login, :alert => "Invalid email or password"
+      end
+  end
+
+  def destroy
+    session[:farmer_id] = nil
+    redirect_to root_path :notice => "You have been logged out"
+  end
 end
