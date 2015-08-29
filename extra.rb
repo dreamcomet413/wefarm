4b75498feebf3df45995d679e94f6b419bbbba4f
pracdev <danielcomet819@gmail.com>
2015-08-30 00:27:52 +0200

    recreated thd instance

--- extra.rb
+++ extra.rb
@@ -1,52 +1,23 @@
-require 'openssl'
+/auth/facebook?display=popup
+      def authorize_params
+        super.tap do |params|
+          %w[display scope auth_type].each do |v|
+            if request.params[v]
+              params[v.to_sym] = request.params[v]
+            end
+          end
 
-module OmniAuth
-  module Facebook
-    class SignedRequest
-      class UnknownSignatureAlgorithmError < NotImplementedError; end
-      SUPPORTED_ALGORITHM = 'HMAC-SHA256'
-
-      attr_reader :value, :secret
-
-      def self.parse(value, secret)
-        new(value, secret).payload
-      end
-
-      def initialize(value, secret)
-        @value = value
-        @secret = secret
-      end
-
-      def payload
-        @payload ||= parse_signed_request
-      end
-
-      private
-
-      def parse_signed_request
-        signature, encoded_payload = value.split('.')
-        return if signature.nil?
-
-        decoded_hex_signature = base64_decode_url(signature)
-        decoded_payload = MultiJson.decode(base64_decode_url(encoded_payload))
-
-        unless decoded_payload['algorithm'] == SUPPORTED_ALGORITHM
-          raise UnknownSignatureAlgorithmError, "unknown algorithm: #{decoded_payload['algorithm']}"
-        end
-
-        if valid_signature?(decoded_hex_signature, encoded_payload)
-          decoded_payload
+          params[:scope] ||= DEFAULT_SCOPE
         end
       end
 
-      def valid_signature?(signature, payload, algorithm = OpenSSL::Digest::SHA256.new)
-        OpenSSL::HMAC.digest(algorithm, secret, payload) == signature
-      end
+      protected
 
-      def base64_decode_url(value)
-        value += '=' * (4 - value.size.modulo(4))
-        Base64.decode64(value.tr('-_', '+/'))
-      end
+      def build_access_token
+        super.tap do |token|
+          token.options.merge!(access_token_options)
+        end
+      end      
     end
   end
 end
